100496f721adf8ec7893abdaa2e4aaa20a7d8b2b
juliaraamos <rodrigsjulia@gmail.com>
2021-07-26 15:15:57 -0300

    paginação na pesquisa de cervejas

--- src/main/java/com/julia/brewer/config/WebConfig.java
+++ src/main/java/com/julia/brewer/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.format.number.NumberStyleFormatter;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
@@ -35,6 +36,7 @@ import nz.net.ultraq.thymeleaf.LayoutDialect;
 @ComponentScan(basePackageClasses = { CervejasController.class })
 // essa classe vai ensinar o spring a encontrar os controllers
 @EnableWebMvc
+@EnableSpringDataWebSupport
 public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
 
 	private ApplicationContext applicationContext;
--- src/main/java/com/julia/brewer/repository/helper/cerveja/CervejasQueries.java
+++ src/main/java/com/julia/brewer/repository/helper/cerveja/CervejasQueries.java
@@ -2,11 +2,13 @@ package com.julia.brewer.repository.helper.cerveja;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+
 import com.julia.brewer.model.Cerveja;
 import com.julia.brewer.repository.filter.CervejaFilter;
 
 public interface CervejasQueries {
 
-	public List<Cerveja> filtrar(CervejaFilter filtro);
+	public List<Cerveja> filtrar(CervejaFilter filtro, Pageable pageable);
 	
 }
