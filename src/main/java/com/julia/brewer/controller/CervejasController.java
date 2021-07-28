46b34e19eabffdf8d3ade237b4e7910b10f2ff83
juliaraamos <rodrigsjulia@gmail.com>
2021-07-28 18:00:49 -0300

    mantendo o filtro nas páginas

--- src/main/java/com/julia/brewer/controller/CervejasController.java
+++ src/main/java/com/julia/brewer/controller/CervejasController.java
@@ -1,9 +1,9 @@
 package com.julia.brewer.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.julia.brewer.controller.page.PageWrapper;
 import com.julia.brewer.model.Cerveja;
 import com.julia.brewer.model.Origem;
 import com.julia.brewer.model.Sabor;
@@ -64,14 +65,15 @@ public class CervejasController {
 		}
 	
 	@GetMapping
-	public ModelAndView pesquisar(CervejaFilter cervejaFilter, BindingResult result, @PageableDefault(size = 2) Pageable pageable) {
+	public ModelAndView pesquisar(CervejaFilter cervejaFilter, BindingResult result, 
+			@PageableDefault(size = 2) Pageable pageable, HttpServletRequest httpServletRequest) {
 		ModelAndView mv = new ModelAndView("cerveja/PesquisaCervejas");
 		mv.addObject("estilos", estilos.findAll());
 		mv.addObject("sabores", Sabor.values());
 		mv.addObject("origens", Origem.values());
 		
-		Page<Cerveja> pagina = cervejas.filtrar(cervejaFilter, pageable);
-		mv.addObject("pagina", pagina);
+		PageWrapper<Cerveja> paginaWrapper = new PageWrapper<>(cervejas.filtrar(cervejaFilter, pageable), httpServletRequest);
+		mv.addObject("pagina", paginaWrapper);
 		return mv;
 	}
 }
