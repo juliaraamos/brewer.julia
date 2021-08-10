fb6433c7ba84425f541353b810c586182edcc726
juliaraamos <rodrigsjulia@gmail.com>
2021-08-10 15:02:55 -0300

    migração de estados concluída

--- src/main/java/com/julia/brewer/controller/ClientesController.java
+++ src/main/java/com/julia/brewer/controller/ClientesController.java
@@ -1,23 +1,28 @@
 package com.julia.brewer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.julia.brewer.model.Cliente;
 import com.julia.brewer.model.TipoPessoa;
+import com.julia.brewer.repository.Estados;
 
 @Controller
 @RequestMapping("/clientes")
 public class ClientesController {
 	
+	@Autowired
+	private Estados estados;
 	
-	@RequestMapping("/novo")
+	@RequestMapping("novo")
 	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView("cliente/CadastroCliente");
 		mv.addObject("tiposPessoa", TipoPessoa.values());	
+		mv.addObject("estados", estados.findAll());
 		return mv;
 	}
+	
 	
 	
 }
---
+++ src/main/java/com/julia/brewer/repository/Estados.java
@@ -0,0 +1,9 @@
+package com.julia.brewer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.julia.brewer.model.Estado;
+
+public interface Estados extends JpaRepository<Estado, Long>{
+
+}
